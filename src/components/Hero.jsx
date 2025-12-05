0ff9d594e2f55d0eb581ede10849e5da108bca19
Angeline Flores De Guzman <angelinefloresdeguzman@gmail.com>
2025-12-05 23:26:40 +0800

    Test auto deploy

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,7 +9,7 @@ export default function Hero() {
         href="#projects"
         className="px-6 py-3 bg-blue-500 text-white rounded hover:bg-blue-600 transition"
       >
-        View My Work
+        View My Works
       </a>
     </section>
   );
